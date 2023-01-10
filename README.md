93a944fabd361b7f98d4511d6ae4c0ace3c31951
emanuelefavero <emafaux89@gmail.com>
2023-01-10 13:23:28 +0100

    add second apple-like effect

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# **Apple scroll animation**
+# Apple scroll animation
 
 This is a vanilla Javascript way of doing the "sync video/image frame to scroll" animation often found on _Apple_ website.
+
+> Note: As a second apple-like effect, an element will be shown when the user scrolls to the bottom of the page.
--- script.js
+++ script.js
@@ -1,18 +1,20 @@
+// FIRST APPLE-LIKE EFFECT:
+// Sync video with scroll
 const html = document.documentElement
 const canvas = document.getElementById('hero-lightpass')
 const context = canvas.getContext('2d')
 
 const frameCount = 148
 const currentFrame = (index) =>
-    `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${index
-        .toString()
-        .padStart(4, '0')}.jpg`
+  `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${index
+    .toString()
+    .padStart(4, '0')}.jpg`
 
 const preloadImages = () => {
-    for (let i = 1; i < frameCount; i++) {
-        const img = new Image()
-        img.src = currentFrame(i)
-    }
+  for (let i = 1; i < frameCount; i++) {
+    const img = new Image()
+    img.src = currentFrame(i)
+  }
 }
 
 const img = new Image()
@@ -20,24 +22,37 @@ img.src = currentFrame(1)
 canvas.width = 1158
 canvas.height = 770
 img.onload = function () {
-    context.drawImage(img, 0, 0)
+  context.drawImage(img, 0, 0)
 }
 
 const updateImage = (index) => {
-    img.src = currentFrame(index)
-    context.drawImage(img, 0, 0)
+  img.src = currentFrame(index)
+  context.drawImage(img, 0, 0)
 }
 
 window.addEventListener('scroll', () => {
-    const scrollTop = html.scrollTop
-    const maxScrollTop = html.scrollHeight - window.innerHeight
-    const scrollFraction = scrollTop / maxScrollTop
-    const frameIndex = Math.min(
-        frameCount - 1,
-        Math.ceil(scrollFraction * frameCount)
-    )
+  const scrollTop = html.scrollTop
+  const maxScrollTop = html.scrollHeight - window.innerHeight
+  const scrollFraction = scrollTop / maxScrollTop
+  const frameIndex = Math.min(
+    frameCount - 1,
+    Math.ceil(scrollFraction * frameCount)
+  )
 
-    requestAnimationFrame(() => updateImage(frameIndex + 1))
+  requestAnimationFrame(() => updateImage(frameIndex + 1))
 })
 
 preloadImages()
+
+// SECOND APPLE-LIKE EFFECT:
+// Show element only if the has scrolled to the bottom of the page
+const bottomPageElement = document.querySelector('.bottom-page-element')
+window.onscroll = function (ev) {
+  if (window.innerHeight + window.scrollY >= document.body.scrollHeight) {
+    // you're at the bottom of the page
+    console.log('Bottom of page')
+    bottomPageElement.classList.add('show')
+  } else {
+    bottomPageElement.classList.remove('show')
+  }
+}
